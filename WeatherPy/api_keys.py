a6dc6d91c8645139310edf5a7d8e91cb37a1ff42
Fereshteh Aghaei <fereshtehaghaei17@yahoo.com>
2020-10-19 13:15:11 -0600

    Created base_url, query_url, request.get and JSON

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "7f07de32089f0bb96b29d09af6bab9e7"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCSfuj-SbUhJftqsTU-dVAeBs-tXC9oU70"
